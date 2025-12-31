70a444c68f1b85ed6b71c63b14eab42ef1bbd501
WildKadeGaming <wildkadegaming@gmail.com>
2025-12-30 20:12:49 -0700

    Release 0.3.0a1

--- hikariwave/__init__.py
+++ hikariwave/__init__.py
@@ -1,8 +1,8 @@
 """
-### hikari-wave: `0.2.0a1`\n
+### hikari-wave: `0.3.0a1`\n
 A lightweight, native voice implementation for `hikari`-based Discord bots.
 
-**Documentation:** https://hikari-wave.wildevstudios.net/en/0.2.0a1\n
+**Documentation:** https://hikari-wave.wildevstudios.net/en/0.3.0a1\n
 **GitHub:** https://github.com/WilDev-Studios/hikari-wave
 """
 
@@ -12,5 +12,6 @@ from hikariwave.client import VoiceClient
 from hikariwave.config import Config, BufferConfig, BufferMode
 from hikariwave.connection import VoiceConnection
 from hikariwave.event.events import *
+from hikariwave.event.types import VoiceWarningType
 from hikariwave.internal.error import *
 from hikariwave.internal.result import Result, ResultReason
