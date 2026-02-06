5577ecea52c06640938b4e7b7ba8e964425bd686
WildKadeGaming <wildkadegaming@gmail.com>
2026-02-06 12:10:26 -0700

    Cache documentation

--- hikariwave/impl/cache.py
+++ hikariwave/impl/cache.py
@@ -26,13 +26,30 @@ Muted:    TypeAlias = bool
 
 @dataclass(slots=True)
 class VoiceChannelMetadata:
+    """Metadata container for a voice channel."""
+
     active: set[hikari.Snowflake]
+    """A set of all actively speaking users in this channel."""
     guild_id: hikari.Snowflake
+    """The ID of the guild this channel is in."""
     id: hikari.Snowflake
+    """The ID of this channel."""
     members: dict[hikari.Snowflake, hikari.Member]
+    """All members within this channel."""
 
 class Cache:
+    """Cache management implementation."""
+
     def __init__(self, client: VoiceClient) -> None:
+        """
+        Create a new cache management instance.
+
+        Parameters
+        ----------
+        client : VoiceClient
+            The controlling voice system client.
+        """
+
         self._client: VoiceClient = client
         self._client._bot.subscribe(hikari.VoiceStateUpdateEvent, self.__state_update)
 
@@ -178,9 +195,27 @@ class Cache:
             await self.__member_update(member, state)
 
     def check_ssrc_connected(self, ssrc: int) -> bool:
+        """
+        Check if the provided SSRC maps to a connected user.
+
+        Parameters
+        ----------
+        ssrc : int
+            The SSRC of the user.
+
+        Returns
+        -------
+        bool
+            If the SSRC maps to a user, thus being connected.
+        """
+
         return ssrc in self._ssrcsr
 
     def clear(self) -> None:
+        """
+        Clear all internal data.
+        """
+
         self._channels.clear()
         self._members.clear()
 
@@ -190,12 +225,44 @@ class Cache:
         self._states.clear()
 
     def delete_channel_metadata(self, channel_id: hikari.Snowflake) -> VoiceChannelMetadata | None:
+        """
+        Delete metadata for a specific channel.
+
+        Parameters
+        ----------
+        channel_id : hikari.Snowflake
+            The ID of the channel to clear.
+
+        Returns
+        -------
+        VoiceChannelMetadata | None
+            The deleted metadata, if found.
+        """
+
         return self._channels.pop(channel_id, None)
 
     def delete_member(self, member_id: hikari.Snowflake) -> None:
+        """
+        Delete a member mapping to a channel.
+
+        Parameters
+        ----------
+        member_id : hikari.Snowflake
+            The ID of the member to remove.
+        """
+
         del self._members[member_id]
 
     def delete_ssrc(self, member_id: hikari.Snowflake) -> None:
+        """
+        Delete/disassociate a SSRC with a member.
+
+        Parameters
+        ----------
+        member_id : hikari.Snowflake
+            The ID of the member to disassociate SSRCs with.
+        """
+
         ssrc: int | None = self._ssrcs.pop(member_id, None)
 
         if not ssrc:
@@ -204,6 +271,20 @@ class Cache:
         self._ssrcsr.pop(ssrc, None)
 
     def get_channel_member_ids(self, channel_id: hikari.Snowflake) -> list[hikari.Snowflake]:
+        """
+        Get a list of all members within a channel.
+
+        Parameters
+        ----------
+        channel_id : hikari.Snowflake
+            The ID of the channel.
+
+        Returns
+        -------
+        list[hikari.Snowflake]
+            All members within the channel.
+        """
+
         metadata: VoiceChannelMetadata | None = self._channels.get(channel_id)
         if not metadata:
             return []
@@ -211,23 +292,111 @@ class Cache:
         return metadata.members.keys()
 
     def get_channel_metadata(self, channel_id: hikari.Snowflake) -> VoiceChannelMetadata | None:
+        """
+        Get the metadata for a channel.
+
+        Parameters
+        ----------
+        channel_id : hikari.Snowflake
+            The ID of the channel.
+
+        Returns
+        -------
+        VoiceChannelMetadata | None
+            The channel metadata, if found.
+        """
+
         return self._channels.get(channel_id)
 
     def get_member_channel(self, member_id: hikari.Snowflake) -> hikari.Snowflake | None:
+        """
+        Get the channel a member is in.
+
+        Parameters
+        ----------
+        member_id : hikari.Snowflake
+            The ID of the member.
+
+        Returns
+        -------
+        hikari.Snowflake | None
+            The ID of the channel, if found.
+        """
+
         return self._members.get(member_id)
 
     def get_ssrc_member(self, ssrc: int) -> hikari.Snowflake | None:
+        """
+        Get the member associated with a SSRC.
+
+        Parameters
+        ----------
+        ssrc : int
+            The SSRC of the member.
+
+        Returns
+        -------
+        hikari.Snowflake | None
+            The ID of the member associated with this SSRC.
+        """
+
         return self._ssrcsr.get(ssrc)
 
     def set_channel_metadata(self, channel_id: hikari.Snowflake, metadata: VoiceChannelMetadata) -> None:
+        """
+        Set the metadata for a channel.
+
+        Parameters
+        ----------
+        channel_id : hikari.Snowflake
+            The ID of the channel.
+        metadata : VoiceChannelMetadata
+            The metadata for this channel.
+        """
+
         self._channels[channel_id] = metadata
 
     def set_member_channel(self, member_id: hikari.Snowflake, channel_id: hikari.Snowflake) -> None:
+        """
+        Set the channel that a member is in.
+
+        Parameters
+        ----------
+        member_id : hikari.Snowflake
+            The ID of the member.
+        channel_id : hikari.Snowflake
+            The ID of the channel.
+        """
+
         self._members[member_id] = channel_id
 
     def set_member_ssrc(self, member_id: hikari.Snowflake, ssrc: int) -> None:
+        """
+        Associate a SSRC with a member.
+
+        Parameters
+        ----------
+        member_id : hikari.Snowflake
+            The ID of the member.
+        ssrc : int
+            The SSRC to associate.
+        """
+
         self._ssrcs[member_id] = ssrc
         self._ssrcsr[ssrc] = member_id
 
     def set_member_state(self, member_id: hikari.Snowflake, mute: bool, deaf: bool) -> None:
+        """
+        Set the state of a member.
+
+        Parameters
+        ----------
+        member_id : hikari.Snowflake
+            The ID of the member.
+        mute : bool
+            If this member is muted.
+        deaf : bool
+            If this member is deafened.
+        """
+
         self._states[member_id] = (mute, deaf)
